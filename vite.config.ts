c133fbf210e25784cd83252a37fb838b6644e497
Matt Frank <matthew.i.frank@gmail.com>
2026-01-29 21:58:15 -0600

    make it live on github pages

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        app: resolve(__dirname, 'app/index.html'),
+      },
+    },
+  },
+})
